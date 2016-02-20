411a923f0ec680b278467763cad59acc93880edc
Mohamed Jassim <jassim.al.azmi@gmail.com>
2016-02-20 08:11:24 +0300

    Fixes bugs
    -- Corrects the file path.
    -- Changes spelling of loads to load and dumps to dump.
    -- Adds self parameter to the send and receive method.

--- mars/mars.py
+++ mars/mars.py
@@ -6,23 +6,22 @@ class Mars(object):
     def __init__(self, doc):
         self.doc = doc
     
-    def send(my_dict):
+    def send(self, my_dict):
         
         with open (self.doc,'w') as f:
-            json.dumps(my_dict, f)
-        
+            json.dump(my_dict, f)
+            
         curdir = os.path.dirname(__file__)
-        if os.path.exists(os.path.join(curdir, '..'+self.doc)):
-            if os.stat(self.doc)st_size > 1:
+        if os.path.exists(os.path.join(curdir, self.doc)) and os.stat(self.doc).st_size > 1 :
                 return 1
         else:
             raise Error('could not create the file')
 
-    def receive():
+    def receive(self):
         curdir = os.path.dirname(__file__)
-        if os.path.exists(os.path.join(curdir, '..'+self.doc)):
+        if os.path.exists(os.path.join(curdir, self.doc)):
             with open(self.doc,'r') as f:
-                my_dict = json.loads(f)
+                my_dict = json.load(f)
             return my_dict
         else:
             raise Error('File does not exist')
